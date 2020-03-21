8cff1757f7f34af0037a32b83dc195ea4b73a4e3
Manuel Zander <manuel.zander@hotmail.de>
2020-03-21 13:55:54 +0000

    [MZ] Updated main.py with params for file names

--- config.py
+++ config.py
@@ -5,3 +5,6 @@ ROOT_DIR = Path(os.path.dirname(os.path.abspath(__file__)))
 DATA_DIR = Path(os.path.join(ROOT_DIR, "data"))
 TEST_DIR = Path(os.path.join(ROOT_DIR, "tests"))
 TEST_DATA_FILE = "test_data.jsonl"
+API_KEY_FILE = "api_keys.txt"
+PLACE_ID_FILE = "place_ids.txt"
+
--- main.py
+++ main.py
@@ -16,7 +16,7 @@ import populartimes
 from datetime import datetime
 from itertools import cycle
 
-from config import ROOT_DIR, DATA_DIR, TEST_DATA_FILE
+from config import ROOT_DIR, DATA_DIR, TEST_DATA_FILE, API_KEY_FILE, PLACE_ID_FILE
 
 
 # Settings
@@ -33,11 +33,18 @@ coloredlogs.install(level="INFO")
 def get_test_data(place_id):
     try:
         data = pd.read_json(os.path.join(ROOT_DIR, TEST_DATA_FILE), lines=True)
-        response = data[data["id"] == place_id].to_json(orient="records", lines=True)
-        logger.info(
-            f"Success reading file {TEST_DATA_FILE} and getting data for id {place_id}"
+        if len(data[data["id"] == place_id]):
+            response = data[data["id"] == place_id].to_json(
+                orient="records", lines=True
+            )
+            logger.info(
+                f"Success reading file {TEST_DATA_FILE} and getting data for id {place_id}"
+            )
+            return response
+        raise populartimes.crawler.PopulartimesException(
+            "Google Places INVALID_REQUEST",
+            "The query string is malformed, check if your formatting for lat/lng and radius is correct.",
         )
-        return response
     except Exception as e:
         logger.error(f"Error getting data:\n{e}")
         traceback.print_exc(file=sys.stdout)
@@ -55,23 +62,28 @@ def get_data():
     try:
         logger.info(f"Getting data")
 
-        with open("api_keys.txt") as f:
+        if not os.path.isfile(API_KEY_FILE) or not os.path.isfile(PLACE_ID_FILE):
+            logger.error(
+                f"API_KEY_FILE {API_KEY_FILE} or PLACE_ID_FILE {PLACE_ID_FILE} not found"
+            )
+            raise SystemExit
+
+        with open(API_KEY_FILE) as f:
             api_keys = cycle([key.strip() for key in f.readlines()])
 
-        with open("place_ids.txt") as f:
+        with open(PLACE_ID_FILE) as f:
             place_ids = [place_id.strip() for place_id in f.readlines()]
 
         responses = []
 
         for place_id in place_ids:
-            # response = json.loads(call_api(next(api_keys), id)) # prod
-            response = json.loads(get_test_data(place_id)) # dev
-            
+            # response = json.loads(call_api(next(api_keys), "test")) # prod
+            response = json.loads(get_test_data(place_id))  # dev
+
             if "current_popularity" not in response:
-                logger.error(f"Error no current_popularity in data:\n{e}")
+                logger.error(f"Error no current_popularity in data")
                 raise SystemExit
 
-            del response["international_phone_number"]
             response["timestamp"] = time.time()
             responses.append(response)
 
